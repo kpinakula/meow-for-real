02d569ece120074a1931124a6cd29513155370b5
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-07-27 17:58:28 +0100

    Add example tests

--- cypress/e2e/landing_page.cy.js
+++ cypress/e2e/landing_page.cy.js
@@ -1,6 +1,27 @@
 describe('landing page', () => {
-  it('loads locally', () => {
+  it('visits landing page', () => {
     cy.visit('http://localhost:3000/')
-    // expect(true).to.equal(true)
+  })
+
+  // it('should have a title', () => {
+  //   cy.title().should('eq','Verification Service')
+  // })
+})
+
+describe('My First Test', () => {
+  it('Gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify
+    // that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
   })
 })
